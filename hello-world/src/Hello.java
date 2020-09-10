a40c3868c52ba025319967106a0745fb115e6035
santiloopz <santilpzate@gmail.com>
2020-09-10 10:22:38 +0200

    trying to make dynamic string, error with static context

--- hello-world/src/Hello.java
+++ hello-world/src/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
     private String name = "Bro";
+
     public static void main(String[] args) {
-        System.out.println("Hello Bro!");
+        System.out.println("Hello,  ".concat(this.name));
     }
+
 }

a3742296ffccbb41b29b7429156a1380b84da187
santiloopz <santilpzate@gmail.com>
2020-09-11 13:37:51 +0200

    playing with numbers

--- ByteShortIntLong/src/learnjava/Main.java
+++ ByteShortIntLong/src/learnjava/Main.java
@@ -5,7 +5,36 @@ public class Main {
         int min = Integer.MIN_VALUE;
         int max = Integer.MAX_VALUE;
 
-        System.out.println("int min value: " + min);
-        System.out.println("int max value: " + max);
+        System.out.println("int min value: " + min);                // -2147483648
+        System.out.println("int max value: " + max);                // 2147483647
+        System.out.println("overflow max value: " + (max + 1));     // -2147483648
+        System.out.println("overflow min value: " + (min - 1));     // 2147483647
+
+        // you can use underscores to separate numbers
+        int maxNumberSeparated = 2_147_483_647;
+
+        byte minByte = Byte.MIN_VALUE;
+        byte maxByte = Byte.MAX_VALUE;
+
+        System.out.println("int min byte value: " + minByte);                // -128
+        System.out.println("int max byte value: " + maxByte);                // 127
+        System.out.println("overflow max byte value: " + (maxByte + 1));     // -128
+        System.out.println("overflow min byte value: " + (minByte - 1));     // 127
+
+        short minShort = Short.MIN_VALUE;
+        short maxShort = Short.MAX_VALUE;
+
+        System.out.println("int min short value: " + minShort);                // -32769
+        System.out.println("int max short value: " + maxShort);                // 32768
+        System.out.println("overflow max short value: " + (maxShort + 1));     // -32769
+        System.out.println("overflow min short value: " + (minShort - 1));     // 32768
+
+        long minLong = Long.MIN_VALUE;
+        long maxLong = Long.MAX_VALUE;
+
+        System.out.println("int min long value: " + minLong);                // -9223372036854775808
+        System.out.println("int max long value: " + maxLong);                // 9223372036854775807
+        System.out.println("overflow max long value: " + (maxLong + 1));     // -9223372036854775808
+        System.out.println("overflow min long value: " + (minLong - 1));     // 9223372036854775807
     }
 }
--- notes/data-types.md
+++ notes/data-types.md
@@ -3,9 +3,12 @@
 
 ### Primitive data types
 1. boolean
-1. byte
+1. byte --> 8 bits
 1. char
-1. short
-1. int
-1. long
+1. short --> 16 bits
+1. int --> 32 bits
+1. long --> 64 bits
 1. float
+
+### Wrapper classes
+Java uses the concept of `wrapper class` for the primitive values, for example in the case of `int`, we got the wrapper class `Integer`.

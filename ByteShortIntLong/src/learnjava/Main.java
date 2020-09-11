7dfb9112aaf83aede24d150094f2e99c75d08527
santiloopz <santilpzate@gmail.com>
2020-09-11 13:23:53 +0200

    added byte short int long project

---
+++ ByteShortIntLong/src/learnjava/Main.java
@@ -0,0 +1,11 @@
+package learnjava;
+
+public class Main {
+    public static void main(String[] args) {
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+
+        System.out.println("int min value: " + min);
+        System.out.println("int max value: " + max);
+    }
+}

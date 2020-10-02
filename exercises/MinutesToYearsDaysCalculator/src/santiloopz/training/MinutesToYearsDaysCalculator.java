039e37be7bb890ec1aab5c0900545ad10a866f16
santiloopz <santilpzate@gmail.com>
2020-10-02 09:50:13 +0200

    finished exercise

--- exercises/MinutesToYearsDaysCalculator/src/santiloopz/training/MinutesToYearsDaysCalculator.java
+++ exercises/MinutesToYearsDaysCalculator/src/santiloopz/training/MinutesToYearsDaysCalculator.java
@@ -1,4 +1,22 @@
 package santiloopz.training;
 
 public class MinutesToYearsDaysCalculator {
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
+
+    public static void printYearsAndDays(long minutes) {
+        long minutesInADay = 60L * 24L;
+        long minutesInAYear = minutesInADay * 365L;
+        long years = 0;
+        long days = 0;
+
+        if (minutes < 0) {
+            System.out.println(INVALID_VALUE_MESSAGE);
+            return;
+        } else if (minutes >= minutesInADay) {
+            days = (minutes % minutesInAYear) / minutesInADay;
+            years =  minutes / minutesInAYear;
+        }
+
+        System.out.println(minutes + " min = " + years + " y and " + days + " d");
+    }
 }

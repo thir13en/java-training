6061f22ece04b0d44affe1e44a7e3ff93c485955
santiloopz <santilpzate@gmail.com>
2020-10-11 12:31:19 +0200

    finsished switch practise exersice

--- exercises/LeapYearCalculator/src/santiloopz/training/LeapYearCalculator.java
+++ exercises/LeapYearCalculator/src/santiloopz/training/LeapYearCalculator.java
@@ -12,4 +12,20 @@ public class LeapYearCalculator {
         }
         return false;
     }
+    
+    public static int getDaysInMonth(int month, int year) {
+        if (year < 1 || year > 9999) {
+            return -1;
+        }
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                return 31;
+            case 2:
+                return isLeapYear(year) ? 29 : 28;
+            case 4: case 6: case 9: case 11:
+                return 30;
+            default:
+                return -1;
+        }
+    }
 }

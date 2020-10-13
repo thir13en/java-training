e66014eec681d3d53456c0b007e8d831c86b87d3
santiloopz <santilpzate@gmail.com>
2020-10-13 08:11:36 +0200

    finished sum odd range exercise

---
+++ exercises/SumOddRange/src/com/santiloopz/learning/SumOddRange.java
@@ -0,0 +1,20 @@
+package com.santiloopz.learning;
+
+public class SumOddRange {
+    public static boolean isOdd(int number) {
+        return number >= 0 && number % 2 != 0;
+    }
+
+    public static int sumOdd(int start, int end) {
+        int sum = 0;
+        if (start < 0 || end < 0 || start > end) {
+            return -1;
+        }
+        for (int i=start; i<=end; i++) {
+            if (isOdd(i)) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}

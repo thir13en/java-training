9a2ba3f001036f998c47fc76135add53e4c852be
santiloopz <santilpzate@gmail.com>
2020-10-16 08:31:17 +0200

    finished is palindrome exercise

--- exercises/NumberPalindrome/src/com/santiloopz/learning/Main.java
+++ exercises/NumberPalindrome/src/com/santiloopz/learning/Main.java
@@ -3,6 +3,6 @@ package com.santiloopz.learning;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
+	    NumberPalindrome.isPalindrome(-222);
     }
 }
--- exercises/NumberPalindrome/src/com/santiloopz/learning/NumberPalindrome.java
+++ exercises/NumberPalindrome/src/com/santiloopz/learning/NumberPalindrome.java
@@ -2,4 +2,20 @@ package com.santiloopz.learning;
 
 
 public class NumberPalindrome {
+    public static boolean isPalindrome(int number) {
+        number *= number < 0 ? -1 : 1;
+        int buffer = number;
+        int reversed = 0;
+
+        while (buffer > 0) {
+            reversed += buffer % 10;
+            buffer /= 10;
+            System.out.println(reversed);
+            if (buffer > 0) {
+                reversed *= 10;
+            }
+        }
+
+        return reversed == number;
+    }
 }

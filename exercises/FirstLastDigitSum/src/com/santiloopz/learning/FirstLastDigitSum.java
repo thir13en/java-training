73fce8237062316c86f776bbf64173acc8fc80b9
santiloopz <santilpzate@gmail.com>
2020-10-17 13:46:25 +0200

    finished exercise and prepared next one

--- exercises/FirstLastDigitSum/src/com/santiloopz/learning/FirstLastDigitSum.java
+++ exercises/FirstLastDigitSum/src/com/santiloopz/learning/FirstLastDigitSum.java
@@ -1,4 +1,22 @@
 package com.santiloopz.learning;
 
+
 public class FirstLastDigitSum {
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        };
+        int first = 0;
+        int last = number % 10;
+
+        do {
+            if (number < 10) {
+                first = number;
+            }
+            number /= 10;
+        }
+        while (number > 0);
+
+        return last + first;
+    }
 }

586724d4bac9b32aff313969044a5d29a37849c7
santiloopz <santilpzate@gmail.com>
2020-10-20 08:27:23 +0200

    finished exercise and created next one

--- exercises/LastDigitChecker/src/com/santiloopz/learning/LastDigitChecker.java
+++ exercises/LastDigitChecker/src/com/santiloopz/learning/LastDigitChecker.java
@@ -2,7 +2,18 @@ package com.santiloopz.learning;
 
 
 public class LastDigitChecker {
-    public static boolean hasSameLastDigit() {
-        return false;
+
+    public static boolean isValid(int number) {
+        return number > 9 && number < 1001;
+    }
+    public static boolean hasSameLastDigit(int first, int second, int third) {
+        if (!isValid(first) || !isValid(second) || !isValid(third)) {
+            return false;
+        }
+        first %= 10;
+        second %= 10;
+        third %= 10;
+
+        return first == second || first == third || second == third;
     }
 }

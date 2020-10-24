a262090118e01362c21317fd86d0b07f942b752b
santiloopz <santilpzate@gmail.com>
2020-10-24 11:52:41 +0200

    finished exercise and prepared next one

--- exercises/PerfectNumber/src/com/santiloopz/learning/PerfectNumber.java
+++ exercises/PerfectNumber/src/com/santiloopz/learning/PerfectNumber.java
@@ -1,4 +1,19 @@
 package com.santiloopz.learning;
 
 public class PerfectNumber {
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+
+        int divisorsSum = 0;
+
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                divisorsSum += i;
+            }
+        }
+
+        return divisorsSum == number;
+    }
 }

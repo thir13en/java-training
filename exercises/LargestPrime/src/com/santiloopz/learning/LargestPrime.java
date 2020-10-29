a54d1c4b5deb4ae4706c7d95fd814a5002c68f96
santiloopz <santilpzate@gmail.com>
2020-10-29 07:38:58 +0100

    finished exercise and prepared new one

--- exercises/LargestPrime/src/com/santiloopz/learning/LargestPrime.java
+++ exercises/LargestPrime/src/com/santiloopz/learning/LargestPrime.java
@@ -1,4 +1,17 @@
 package com.santiloopz.learning;
 
 public class LargestPrime {
+    public static int getLargestPrime(int number) {
+        if (number < 2) {
+            return -1;
+        }
+
+        for (int i=2; i < number; i++) {
+            while (number % i == 0 && number > 2) {
+                number /= i;
+            }
+        }
+
+        return number;
+    }
 }

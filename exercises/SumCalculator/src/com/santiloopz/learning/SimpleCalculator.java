d5bbd882de55682cc2b12a35a2fcab3e956bff45
santiloopz <santilpzate@gmail.com>
2020-11-09 09:07:05 +0100

    working on exercise

---
+++ exercises/SumCalculator/src/com/santiloopz/learning/SimpleCalculator.java
@@ -0,0 +1,22 @@
+package com.santiloopz.learning;
+
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+}

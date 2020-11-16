d344aa2b4936bcb7dc7587347564eae890460a52
santiloopz <santilpzate@gmail.com>
2020-11-16 08:27:02 +0100

    finished exercise and started next one

--- exercises/Learning/src/com/santiloopz/learning/oop/Point.java
+++ exercises/Learning/src/com/santiloopz/learning/oop/Point.java
@@ -1,4 +1,41 @@
 package com.santiloopz.learning.oop;
 
 public class Point {
+    private int x;
+    private int y;
+
+    public Point() {}
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt(Math.pow((this.x - x), 2) + Math.pow((this.y - y), 2));
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt(Math.pow((this.x - point.x), 2) + Math.pow((this.y - point.y), 2));
+    }
 }

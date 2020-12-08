631c2deff5e8c56b6f5b690661fd96e22e38539e
santiloopz <santilpzate@gmail.com>
2020-12-08 14:50:32 +0100

    finished cylinder exercise

---
+++ exercises/Learning/src/com/santiloopz/learning/oop/cilinder/Circle.java
@@ -0,0 +1,17 @@
+package com.santiloopz.learning.oop.cilinder;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius > 0 ? radius : 0;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+}
---
+++ exercises/Learning/src/com/santiloopz/learning/oop/cilinder/Cylinder.java
@@ -0,0 +1,18 @@
+package com.santiloopz.learning.oop.cilinder;
+
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height > 0 ? height : 0;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
+}

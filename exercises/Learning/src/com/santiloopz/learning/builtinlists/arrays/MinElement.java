2d8359fda5ed3bc4e2762ab18a881d781be14991
santiloopz <santilpzate@gmail.com>
2020-12-21 09:51:35 +0100

    doing exercise

--- exercises/Learning/src/com/santiloopz/learning/builtinlists/arrays/MinElement.java
+++ exercises/Learning/src/com/santiloopz/learning/builtinlists/arrays/MinElement.java
@@ -1,2 +1,15 @@
-package com.santiloopz.learning.builtinlists.arrays;public class MinElement {
+package com.santiloopz.learning.builtinlists.arrays;
+
+import java.util.Scanner;
+
+public class MinElement {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int readInteger() {
+        System.out.println("Add a number");
+        if (scanner.hasNextInt()) {
+            scanner.nextInt();
+        }
+        return 0;
+    }
 }

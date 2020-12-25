c5e2d32c542616230af1b22d6db00a8f00453a19
santiloopz <santilpzate@gmail.com>
2020-12-25 19:18:50 +0100

    finished array reverse exercise

--- exercises/Learning/src/com/santiloopz/learning/builtinlists/arrays/Reverse.java
+++ exercises/Learning/src/com/santiloopz/learning/builtinlists/arrays/Reverse.java
@@ -1,7 +1,21 @@
 package com.santiloopz.learning.builtinlists.arrays;
 
+import java.util.Arrays;
+
 public class Reverse {
-    public static void reverse(int[] numbers) {
-        
+    public static void main(String[] args) {
+        int[] n = { 4, 6, 7, 8, 9, 1 };
+        reverse(n);
+    }
+    private static void reverse(int[] numbers) {
+        int temp;
+
+        for (int i=0; i<(numbers.length / 2); i++) {
+            temp = numbers[i];
+            numbers[i] = numbers[numbers.length - i - 1];
+            numbers[numbers.length - i - 1] = temp;
+        }
+
+        System.out.println("Reversed array = " + Arrays.toString(numbers));
     }
 }

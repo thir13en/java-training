360930959452b060b15e2129b2caf431ecc64fdc
santiloopz <santilpzate@gmail.com>
2021-01-10 17:07:45 +0100

    working on hamburguer exercise

--- exercises/Learning/src/com/santiloopz/learning/oop/billburgers/Hamburger.java
+++ exercises/Learning/src/com/santiloopz/learning/oop/billburgers/Hamburger.java
@@ -1,4 +1,16 @@
 package com.santiloopz.learning.oop.billburgers;
 
 public class Hamburger {
+    private String name;
+    private String meat;
+    private double price;
+    private String breadRollType;
+
+    public Hamburger(String name, String meat, double price, String breadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.breadRollType = breadRollType;
+    }
+
 }

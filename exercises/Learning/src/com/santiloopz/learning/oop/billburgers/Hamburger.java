3016802f4a98ca8ae6e5470cb0165ee56e187b7d
santiloopz <santilpzate@gmail.com>
2021-01-11 08:56:12 +0100

    conitnue working on exercise

--- exercises/Learning/src/com/santiloopz/learning/oop/billburgers/Hamburger.java
+++ exercises/Learning/src/com/santiloopz/learning/oop/billburgers/Hamburger.java
@@ -5,6 +5,14 @@ public class Hamburger {
     private String meat;
     private double price;
     private String breadRollType;
+    private String addition1Name;
+    private double addition1Price;
+    private String addition2Name;
+    private double addition2Price;
+    private String addition3Name;
+    private double addition3Price;
+    private String addition4Name;
+    private double addition4Price;
 
     public Hamburger(String name, String meat, double price, String breadRollType) {
         this.name = name;
@@ -13,4 +21,19 @@ public class Hamburger {
         this.breadRollType = breadRollType;
     }
 
+    public void addHamburgerAddition1(String name, double price) {
+
+    }
+
+    public void addHamburgerAddition2(String name, double price) {
+
+    }
+
+    public void addHamburgerAddition3(String name, double price) {
+
+    }
+
+    public void addHamburgerAddition4(String name, double price) {
+
+    }
 }

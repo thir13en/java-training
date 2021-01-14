31738cc905bea6aeb45b71720d6238779f5f3d97
santiloopz <santilpzate@gmail.com>
2021-01-14 09:12:37 +0100

    finished Deluxe Burger requirements

--- exercises/Learning/src/com/santiloopz/learning/oop/billburgers/DeluxeBurger.java
+++ exercises/Learning/src/com/santiloopz/learning/oop/billburgers/DeluxeBurger.java
@@ -3,6 +3,29 @@ package com.santiloopz.learning.oop.billburgers;
 public class DeluxeBurger extends Hamburger{
     public DeluxeBurger() {
         super("Deluxe Burger", "Vegan", 19.1, "Seeds");
-        // Add additions
+        this.addition1Name = "Drink";
+        this.addition1Price = 0;
+        this.addition2Name = "Chips";
+        this.addition2Price = 0;
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("No additional items can be added to the Deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        this.addHamburgerAddition1(name, price);
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        this.addHamburgerAddition1(name, price);
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        this.addHamburgerAddition1(name, price);
     }
 }

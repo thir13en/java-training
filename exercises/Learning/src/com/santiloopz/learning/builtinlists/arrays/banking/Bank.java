e9df7a13a6db23e85387d6d35270e1f02ad719e5
santiloopz <santilpzate@gmail.com>
2021-01-20 08:53:07 +0100

    added main structure of arraylists exercise

---
+++ exercises/Learning/src/com/santiloopz/learning/builtinlists/arrays/banking/Bank.java
@@ -0,0 +1,8 @@
+package com.santiloopz.learning.builtinlists.arrays.banking;
+
+import java.util.ArrayList;
+
+public class Bank {
+    private String name;
+    private ArrayList<Branch> branches;
+}

535dfc11e3eee19bdb4ea83260fffefcebc064b6
santiloopz <santilpzate@gmail.com>
2021-01-22 09:14:04 +0100

    adding test cases for upcoming exercise

--- exercises/Learning/src/com/santiloopz/learning/builtinlists/arrays/banking/Main.java
+++ exercises/Learning/src/com/santiloopz/learning/builtinlists/arrays/banking/Main.java
@@ -2,6 +2,25 @@ package com.santiloopz.learning.builtinlists.arrays.banking;
 
 public class Main {
     public static void main (String args[]) {
+        Bank bank = new Bank("Triodos Bank");
 
+        bank.addBranch("Berlin");
+
+        bank.addCustomer("Berlin", "Natalia", 7777.77);
+
+        bank.addBranch("Barcelona");
+
+        bank.addCustomer("Barcelona", "Tesla", 9999.99);
+
+        bank.listCustomers("Barcelona", true);
+        bank.listCustomers("Berlin", true);
+
+        if (!bank.addCustomer("Melbourne", "Filipo", 222222.22)) {
+            System.out.println("Error, Melbourne branch does not exist.");
+        }
+
+        if (!bank.addBranch("Barcelona")) {
+            System.out.println("Error, Barcelona branch already exists.");
+        }
     }
 }

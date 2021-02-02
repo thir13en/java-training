12419f7c74d52e7603dcc887354dbe3fbaaf4520
santiloopz <santilpzate@gmail.com>
2021-02-02 08:45:27 +0100

    [Bank exercise] add constructor and addBranch method to Bank class

--- exercises/Learning/src/com/santiloopz/learning/builtinlists/arrays/banking/Bank.java
+++ exercises/Learning/src/com/santiloopz/learning/builtinlists/arrays/banking/Bank.java
@@ -5,4 +5,13 @@ import java.util.ArrayList;
 public class Bank {
     private String name;
     private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        branches = new ArrayList<Branch>();
+    }
+
+    public boolean addBranch(String branchName) {
+        return true;
+    }
 }

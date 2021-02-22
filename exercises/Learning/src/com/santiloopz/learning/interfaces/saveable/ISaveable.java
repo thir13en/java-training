af0f32e7a7db2a5a2ded521f15ce1b51b38209f5
santiloopz <santilpzate@gmail.com>
2021-02-22 08:37:17 +0100

    Added saveable Interface content and Player WIP

--- exercises/Learning/src/com/santiloopz/learning/interfaces/saveable/ISaveable.java
+++ exercises/Learning/src/com/santiloopz/learning/interfaces/saveable/ISaveable.java
@@ -1,4 +1,8 @@
 package com.santiloopz.learning.interfaces.saveable;
 
+import java.util.List;
+
 public interface ISaveable {
+    List<String> write();
+    void read(List<String> changes);
 }
--- exercises/Learning/src/com/santiloopz/learning/interfaces/saveable/Player.java
+++ exercises/Learning/src/com/santiloopz/learning/interfaces/saveable/Player.java
@@ -1,4 +1,15 @@
 package com.santiloopz.learning.interfaces.saveable;
 
 public class Player {
+    String name;
+    String weapon;
+    int hitPoints;
+    int strength;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.weapon = "Sword";
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
 }
